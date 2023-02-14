77677904ae62c98b43a3a5dd57ca18386d20c4fc
Madina <ozdemirov24050@mail.ru>
2023-02-14 20:47:12 +0100

    reshila

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// "Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее."); 
+int a = 6; 
+
+int b= 9;
+
+int max;
+
+if(a>b)  {
+ max=a; 
+
+Console.WriteLine( max); 
+}
+else
+
+if(a<b)   { max=b;
+   Console.WriteLine( max); 
+}
+
+
+
