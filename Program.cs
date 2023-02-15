7ab5d5221fb24ac523a3902dab079cec80311231
Madina <ozdemirov24050@mail.ru>
2023-02-15 22:32:17 +0100

    practicheskoe zadanie 01

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 ﻿// "Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее."); 
-int a = 6; 
+Console.WriteLine("Vvedite dva chisla a,b ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 
-int b= 9;
 
 int max;
 
 if(a>b)  {
  max=a; 
 
-Console.WriteLine( max); 
+Console.WriteLine( "chislo" + " "+ max); 
 }
 else
 
 if(a<b)   { max=b;
-   Console.WriteLine( max); 
+   Console.WriteLine( "chislo "+ " " + max); 
 }
 
 
